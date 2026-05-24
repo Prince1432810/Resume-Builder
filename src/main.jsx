35a6406fd27ee466e01ea8ffaad09ab981e47d0c
Prince Kumar <princekumar@Mac.lan>
2026-05-24 20:46:19 +0530

    remove redux completely

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,13 @@ import "./index.css";
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
             <BrowserRouter>
-                <Provider store={store}>
                     <App />
-                </Provider>
             </BrowserRouter>
         </ClerkProvider>
     </StrictMode>
